f36e23793c0ac6b7971a29100bacc552b3c8bda8
Vrkadia <roeru1412@gmail.com>
2024-03-15 13:46:42 +0700

    [VE] Init: Init Everywheel

--- KRSBI/KRSBI.ino
+++ KRSBI/KRSBI.ino
@@ -15,31 +15,48 @@
 // depending on how you wire or physcally mount the motor the direction is different.
 // But when you send a signal on RPWM the motor will go one way and when you 
 // send a signal on LPWM, the motor will go the other way.
-int RPWM = 4;  // Digital/PWM pin 5 to the RPWM on the BTS7960
-int LPWM = 5;  // Digital/PWM pin 6 to the LPWM on the BTS7960
+int RPWM1 = 4;  // Digital/PWM pin 5 to the RPWM on the BTS7960
+int LPWM1 = 5;  // Digital/PWM pin 6 to the LPWM on the BTS7960
+// Enable "Left" and "Right" movement for the first wheel
+int L_EN1 = 20;  // connect Digital/PWM pin 7 to L_EN on the BTS7960
+int R_EN1 = 21;  // connect Digital/PWM pin 8 to R_EN on the BTS7960
 
-// Enable "Left" and "Right" movement
-int L_EN = 20;  // connect Digital/PWM pin 7 to L_EN on the BTS7960
-int R_EN = 21;  // connect Digital/PWM pin 8 to R_EN on the BTS7960
+int RPWM2 = 6;  // Digital/PWM pin 5 to the RPWM on the BTS7960
+int LPWM2 = 7;  // Digital/PWM pin 6 to the LPWM on the BTS7960
+// Enable "Left" and "Right" movement for the second wheel
+int L_EN2 = 22;  // connect Digital/PWM pin 7 to L_EN on the BTS7960
+int R_EN2 = 24;  // connect Digital/PWM pin 8 to R_EN on the BTS7960
+
+int RPWM3 = 8;  // Digital/PWM pin 5 to the RPWM on the BTS7960
+int LPWM3 = 9;  // Digital/PWM pin 6 to the LPWM on the BTS7960
+// Enable "Left" and "Right" movement for the third wheel
+int L_EN3 = 26;  // connect Digital/PWM pin 7 to L_EN on the BTS7960
+int R_EN3 = 28;  // connect Digital/PWM pin 8 to R_EN on the BTS7960
+
+
 
 void setup() {
   // put your setup code here, to run once:
 
   // initialize all our pins to output
-  for (int i = 4; i < 22; i++) {
+  for (int i = 4; i < 29; i++) {
     pinMode(i, OUTPUT);
   }
   // set all the pins you want to use to LOW
-  for (int i = 4; i < 22; i++) {
-    digitalWrite(i, HIGH);
+  for (int i = 4; i < 29; i++) {
+    digitalWrite(i, LOW);
   }
   delay(1000);// wait a second
   Serial.begin(9600);
   
   //enable "Right" and "Left" movement on the HBridge
   // Notice use of digitalWrite to simply turn it on and keep it on.
-  digitalWrite(R_EN, HIGH);  
-  digitalWrite(L_EN, HIGH);
+  digitalWrite(R_EN1, HIGH);  
+  digitalWrite(L_EN1, HIGH);
+  digitalWrite(R_EN2, HIGH);  
+  digitalWrite(L_EN2, HIGH);
+  digitalWrite(R_EN3, HIGH);  
+  digitalWrite(L_EN3, HIGH);
 }
 
 
@@ -66,4 +83,8 @@ void loop() {
   // at this point should be no movement.
   
   delay(1000);
+}
+
+void forward(){
+  
 }
