c21d68e17c67446f7c2b76ad178183b25a9242f2
Vrkadia <roeru1412@gmail.com>
2024-03-18 10:13:46 +0700

    Update Code

--- KRSBI/KRSBI.ino
+++ KRSBI/KRSBI.ino
@@ -15,23 +15,17 @@
 // depending on how you wire or physcally mount the motor the direction is different.
 // But when you send a signal on RPWM the motor will go one way and when you 
 // send a signal on LPWM, the motor will go the other way.
-int RPWM1 = 4;  // Digital/PWM pin 5 to the RPWM on the BTS7960
-int LPWM1 = 5;  // Digital/PWM pin 6 to the LPWM on the BTS7960
+int RPWM1 = 7;  // Digital/PWM pin 5 to the RPWM on the BTS7960
+int LPWM1 = 6;  // Digital/PWM pin 6 to the LPWM on the BTS7960
 // Enable "Left" and "Right" movement for the first wheel
-int L_EN1 = 20;  // connect Digital/PWM pin 7 to L_EN on the BTS7960
-int R_EN1 = 21;  // connect Digital/PWM pin 8 to R_EN on the BTS7960
 
-int RPWM2 = 6;  // Digital/PWM pin 5 to the RPWM on the BTS7960
-int LPWM2 = 7;  // Digital/PWM pin 6 to the LPWM on the BTS7960
+int RPWM2 = 5;  // Digital/PWM pin 5 to the RPWM on the BTS7960
+int LPWM2 = 4;  // Digital/PWM pin 6 to the LPWM on the BTS7960
 // Enable "Left" and "Right" movement for the second wheel
-int L_EN2 = 22;  // connect Digital/PWM pin 7 to L_EN on the BTS7960
-int R_EN2 = 24;  // connect Digital/PWM pin 8 to R_EN on the BTS7960
 
-int RPWM3 = 8;  // Digital/PWM pin 5 to the RPWM on the BTS7960
-int LPWM3 = 9;  // Digital/PWM pin 6 to the LPWM on the BTS7960
+int RPWM3 = 3;  // Digital/PWM pin 5 to the RPWM on the BTS7960
+int LPWM3 = 2;  // Digital/PWM pin 6 to the LPWM on the BTS7960
 // Enable "Left" and "Right" movement for the third wheel
-int L_EN3 = 26;  // connect Digital/PWM pin 7 to L_EN on the BTS7960
-int R_EN3 = 28;  // connect Digital/PWM pin 8 to R_EN on the BTS7960
 
 
 
@@ -42,21 +36,12 @@ void setup() {
   for (int i = 4; i < 29; i++) {
     pinMode(i, OUTPUT);
   }
-  // set all the pins you want to use to LOW
-  for (int i = 4; i < 29; i++) {
-    digitalWrite(i, LOW);
-  }
   delay(1000);// wait a second
   Serial.begin(9600);
+  stop();
   
   //enable "Right" and "Left" movement on the HBridge
   // Notice use of digitalWrite to simply turn it on and keep it on.
-  digitalWrite(R_EN1, HIGH);  
-  digitalWrite(L_EN1, HIGH);
-  digitalWrite(R_EN2, HIGH);  
-  digitalWrite(L_EN2, HIGH);
-  digitalWrite(R_EN3, HIGH);  
-  digitalWrite(L_EN3, HIGH);
 }
 
 
@@ -68,50 +53,50 @@ void loop() {
   // signal (PWM) to specific pin at a specific "duty cycle".
   // Valid values are 0 to 255.  0 means always off(or no power) and 255 means always on(full power)
   // https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/// wait 5 seconds, motor continues to move because the analogWrite is still pulsing
-  analogWrite(RPWM, 128); // pulse signal now at 128 (about half power... half of max 255).
+  forward();
+  delay(1000);
+  stop();
+  delay(1000);
 
+  backward();
+  delay(1000);
+  stop();
+  delay(1000);
 
-  // after 5 seconds at half power, stop the motor moving
-  analogWrite(RPWM, 0);
+  right();
   delay(1000);
-  // now start moving in opposite direction.
-  analogWrite(LPWM, 64);
+  stop();
   delay(1000);
-  analogWrite(LPWM, 128);
-  delay(1000);
-  analogWrite(LPWM, 0); // Stop moving in this direction
-  // at this point should be no movement.
-  
-  delay(1000);
+
 }
 
 void forward(){
-  analogWrite(RWPM1, 128);
-  analogWrite(LWPM2, 128);
+  analogWrite(RPWM1, 255);
+  analogWrite(LPWM2, 255);
 }
 
 void backward(){
-  analogWrite(LPWM1, 128);
-  analogWrite(RWPM2, 128);
+  analogWrite(LPWM1, 255);
+  analogWrite(RPWM2, 255);
 }
 
 void right(){
-  analogWrite(RPWM1, 128);
-  analogWrite(RPWM2, 128);
-  analogWrite(LPWM3, 128);
+  analogWrite(RPWM1, 255);
+  analogWrite(RPWM2, 255);
+  analogWrite(LPWM3, 255);
 }
 
 void left(){
-  analogWrite(LPWM1, 128);
-  analogWrite(LPWM2, 128);
-  analogWrite(RWPM3, 128);
+  analogWrite(LPWM1, 255);
+  analogWrite(LPWM2, 255);
+  analogWrite(RPWM3, 255);
 }
 
 void stop(){
   analogWrite(LPWM1, 0);
   analogWrite(LPWM2, 0);
-  analogWrite(LWPM3, 0);
-  analogWrite(RWPM1, 0);
-  analogWrite(RWPM2, 0);
-  analogWrite(RWPM3, 0);
+  analogWrite(LPWM3, 0);
+  analogWrite(RPWM1, 0);
+  analogWrite(RPWM2, 0);
+  analogWrite(RPWM3, 0);
 }
